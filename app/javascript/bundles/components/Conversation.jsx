f231890b18f608b18d2b11a80654d35d75e2c345
Jack Collins <BlueSkyFlyer2@gmail.com>
2018-04-01 00:31:51 -0600

    Clear the message input after sending the message

--- app/javascript/bundles/components/Conversation.jsx
+++ app/javascript/bundles/components/Conversation.jsx
@@ -6,18 +6,16 @@ import ConversationMessages from './ConversationMessages';
 class Conversation extends React.Component {
 
   state = {
-    message: null
+    message: ''
   };
 
   handleMessageChange = (event) => {
-    console.log("Handle message change: ", event);
     this.setState({message: event.target.value});
   };
 
   handleMessageSubmit = () => {
     this.props.sendMessage(this.props.activeUserId, this.props.conversation.id, this.state.message);
     this.setState({message: ''})
-    console.log("End of handleMessageSubmit: ", this.state)
   };
 
   render() {
@@ -25,7 +23,7 @@ class Conversation extends React.Component {
       <div>
         <ConversationMessages messages={this.props.conversation.messages} activeUserId={this.props.activeUserId} />
         <Form>
-          <TextArea autoHeight placeholder={"Type a new message..."} onChange={this.handleMessageChange} />
+          <TextArea autoHeight placeholder={"Type a new message..."} onChange={this.handleMessageChange} value={this.state.message} />
           <Button positive disabled={!this.state.message} onClick={this.handleMessageSubmit}>Submit</Button>
         </Form>
       </div>
